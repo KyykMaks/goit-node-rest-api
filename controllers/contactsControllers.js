f1e8a6c0df1ffdab1859496ac5b45e5c1061a595
KyykMaks <ripperkyyk@gmail.com>
2024-05-29 18:00:42 +0300

    hw2-expresss

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,80 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateById,
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const contact = await getContactById(id);
+    if (contact) {
+      res.status(200).json(contact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await removeContact(id);
+    if (deleted) {
+      res.status(200).json(deleted);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw new HttpError(400, error.message);
+    }
+    const newContact = await addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    res.status(error.statusCode || 500).json({ message: error.message });
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw new HttpError(400, error.message);
+    }
+    const update = await updateById(id, req.body);
+    if (update) {
+      res.status(200).json(update);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(error.statusCode || 500).json({ message: error.message });
+  }
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,56 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.join("db", "contacts.json");
+
+export async function listContacts() {
+  const readResult = await fs.readFile(contactsPath);
+  const resultArray = JSON.parse(readResult);
+  return resultArray;
+}
+
+export async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact || null;
+}
+
+export async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+export async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+export async function updateById(contactId, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  contacts[index] = { ...contacts[index], ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+}
